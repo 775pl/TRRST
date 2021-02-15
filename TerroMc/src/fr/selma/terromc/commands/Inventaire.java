3bc8d8f768eb50e7f9c60615c6a5f5f0f13608e6
Selma EL JABRI <selma.eljabri@epsi.fr>
2021-02-15 15:00:35 +0100

    Ajout de la possibilité d'ouvrir un inventaire partagé

---
+++ TerroMc/src/fr/selma/terromc/commands/Inventaire.java
@@ -0,0 +1,29 @@
+package fr.selma.terromc.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
+
+public class Inventaire implements CommandExecutor {
+	
+	Inventory inv = Bukkit.createInventory(null, InventoryType.CHEST, "§8YInventaire");
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String msg, String[] args) {
+		Player player = (Player)sender;
+
+			if(args.length == 0) {
+				if(inv != null ) {
+
+					player.openInventory(inv);
+				
+				}
+
+			}
+			return false;
+		}
+	}
---
+++ TerroMc/src/fr/selma/terromc/commands/package-info.java
@@ -0,0 +1,1 @@
+package fr.selma.terromc.commands;
